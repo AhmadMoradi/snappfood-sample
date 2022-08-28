47316a7df1450316c55c855280300206abf68214
Ahmad Moradi <Atrosa@live.com>
2022-08-29 04:27:01 +0430

    fix: Fix header z-index issue

--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -6,7 +6,7 @@ const Default = ({ children }) => {
   return (
     <div className="default-layout">
       <header className="h-16">
-        <div className="fixed bg-white h-16 top-0 left-0 right-0 mb-4 px-3 py-4 border-solid border-b border-slate-300">
+        <div className="fixed z-10 bg-white h-16 top-0 left-0 right-0 mb-4 px-3 py-4 border-solid border-b border-slate-300">
           <Link to="/">
             <img src={snappfoodLogo} alt="اسنپ فود" />
           </Link>
